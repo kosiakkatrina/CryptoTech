70c9684f6250c345e9efa09dd058ac0575d1be85
Kat <katrina@madetech.com>
2019-10-04 13:26:55 +0100

    new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading;
-
-
+using Newtonsoft.Json.Linq;
+//using System.Web.Script.Serialization;
 namespace ConsoleApp1
 {
     class Program
@@ -40,9 +40,13 @@ namespace ConsoleApp1
 
                 var client = new WebClient();
                 var body = client.DownloadString(
-                    "https://api.airtable.com/v0/appxiufWMNuWFhrKN/Marketplace?api_key=keyviCCmCsS89byTt");
-                var formatted_body = "{\"blocks\": [{ \"type\": \"section\",\"text\": { \"type\": \"mrkdwn\",\"text\": \"Stuff Written down\"}}]}";
-                
+                    "https://api.airtable.com/v0/appxiufWMNuWFhrKN/Marketplace?fields%5B%5D=Name&api_key=API_KEY");
+
+               // var better_string = Formatted(body);
+                var formatted_body = "{\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"Stuff Written down\"} },{\"type\": \"image\",\"title\": {\"type\": \"plain_text\",\"text\": \"Example Image\",\"emoji\": true}, \"image_url\": \"https://api.slack.com/img/blocks/bkb_template_images/goldengate.png\",\"alt_text\": \"Example Image\"}, {\"type\": \"image\",\"title\": {\"type\": \"plain_text\",\"text\": \"image1\",\"emoji\": true}, \"image_url\": \"https://api.slack.com/img/blocks/bkb_template_images/beagle.png\",\"alt_text\": \"image1\"}] }";
+                //formatted_body = formatted_body.Replace("blocks", "blo");
+
+                var last_string = Format(body);
             //Console.Write(body);
                 byte[] responseArray = Encoding.UTF8.GetBytes(formatted_body); // get the bytes to response
                 response.AddHeader("Content-type", "application/json");
@@ -51,8 +55,22 @@ namespace ConsoleApp1
                 response.Close(); // close the connection
                 Console.WriteLine("Response given to a request.");
             }
+
+           // static JObject Formatted(String str)
+          //  {
+                /*JObject json = JObject.Parse(str);
+                json.Property("createdTime").RemoveAll();
+                string result = new JavaScriptSerializer().Serialize(json.Data);
+                json*/
+                //return json;
+            //}
             
-            
+            static String Format(String str)
+            {
+                return str;
+            }
+
+
         }
     }
 }
